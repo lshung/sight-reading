38127b383863533f82d52c9c25b619021e1fbb33
Hung <ls@hung.vn>
2024-02-29 21:08:04 +0700

    Fix minor css and show answer in both English name and Solfege name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,122 @@
+var midi_notes = {
+	127: {organ_key_number: 0, piano_key_number: 0, note_name: 'G9', solfege_name: 'Sol9', frequency: 12543.85},
+	126: {organ_key_number: 0, piano_key_number: 0, note_name: 'F#9/Gb9', solfege_name: 'Fa#9/Solb9', frequency: 11839.82},
+	125: {organ_key_number: 0, piano_key_number: 0, note_name: 'F9', solfege_name: 'Fa9', frequency: 11175.3},
+	124: {organ_key_number: 0, piano_key_number: 0, note_name: 'E9', solfege_name: 'Mi9', frequency: 10548.08},
+	123: {organ_key_number: 0, piano_key_number: 0, note_name: 'D#9/Eb9', solfege_name: 'Re#9/Mib9', frequency: 9956.06},
+	122: {organ_key_number: 0, piano_key_number: 0, note_name: 'D9', solfege_name: 'Re9', frequency: 9397.27},
+	121: {organ_key_number: 0, piano_key_number: 0, note_name: 'C#9/Db9', solfege_name: 'Do#9/Reb9', frequency: 8869.84},
+	120: {organ_key_number: 0, piano_key_number: 0, note_name: 'C9', solfege_name: 'Do9', frequency: 8372.02},
+	119: {organ_key_number: 0, piano_key_number: 0, note_name: 'B8', solfege_name: 'Si8', frequency: 7902.13},
+	118: {organ_key_number: 0, piano_key_number: 0, note_name: 'A#8/Bb8', solfege_name: 'La#8/Sib8', frequency: 7458.62},
+	117: {organ_key_number: 0, piano_key_number: 0, note_name: 'A8', solfege_name: 'La8', frequency: 7040},
+	116: {organ_key_number: 0, piano_key_number: 0, note_name: 'G#8/Ab8', solfege_name: 'Sol#8/Lab8', frequency: 6644.88},
+	115: {organ_key_number: 0, piano_key_number: 0, note_name: 'G8', solfege_name: 'Sol8', frequency: 6271.93},
+	114: {organ_key_number: 0, piano_key_number: 0, note_name: 'F#8/Gb8', solfege_name: 'Fa#8/Solb8', frequency: 5919.91},
+	113: {organ_key_number: 0, piano_key_number: 0, note_name: 'F8', solfege_name: 'Fa8', frequency: 5587.65},
+	112: {organ_key_number: 0, piano_key_number: 0, note_name: 'E8', solfege_name: 'Mi8', frequency: 5274.04},
+	111: {organ_key_number: 0, piano_key_number: 0, note_name: 'D#8/Eb8', solfege_name: 'Re#8/Mib8', frequency: 4978.03},
+	110: {organ_key_number: 0, piano_key_number: 0, note_name: 'D8', solfege_name: 'Re8', frequency: 4698.64},
+	109: {organ_key_number: 0, piano_key_number: 0, note_name: 'C#8/Db8', solfege_name: 'Do#8/Reb8', frequency: 4434.92},
+	108: {organ_key_number: 0, piano_key_number: 88, note_name: 'C8', solfege_name: 'Do8', frequency: 4186.01},
+	107: {organ_key_number: 0, piano_key_number: 87, note_name: 'B7', solfege_name: 'Si7', frequency: 3951.07},
+	106: {organ_key_number: 0, piano_key_number: 86, note_name: 'A#7/Bb7', solfege_name: 'La#7/Sib7', frequency: 3729.31},
+	105: {organ_key_number: 0, piano_key_number: 85, note_name: 'A7', solfege_name: 'La7', frequency: 3520},
+	104: {organ_key_number: 0, piano_key_number: 84, note_name: 'G#7/Ab7', solfege_name: 'Sol#7/Lab7', frequency: 3322.44},
+	103: {organ_key_number: 0, piano_key_number: 83, note_name: 'G7', solfege_name: 'Sol7', frequency: 3135.96},
+	102: {organ_key_number: 0, piano_key_number: 82, note_name: 'F#7/Gb7', solfege_name: 'Fa#7/Solb7', frequency: 2959.96},
+	101: {organ_key_number: 0, piano_key_number: 81, note_name: 'F7', solfege_name: 'Fa7', frequency: 2793.83},
+	100: {organ_key_number: 0, piano_key_number: 80, note_name: 'E7', solfege_name: 'Mi7', frequency: 2637.02},
+	99: {organ_key_number: 0, piano_key_number: 79, note_name: 'D#7/Eb7', solfege_name: 'Re#7/Mib7', frequency: 2489.02},
+	98: {organ_key_number: 0, piano_key_number: 78, note_name: 'D7', solfege_name: 'Re7', frequency: 2349.32},
+	97: {organ_key_number: 0, piano_key_number: 77, note_name: 'C#7/Db7', solfege_name: 'Do#7/Reb7', frequency: 2217.46},
+	96: {organ_key_number: 61, piano_key_number: 76, note_name: 'C7', solfege_name: 'Do7', frequency: 2093},
+	95: {organ_key_number: 60, piano_key_number: 75, note_name: 'B6', solfege_name: 'Si6', frequency: 1975.53},
+	94: {organ_key_number: 59, piano_key_number: 74, note_name: 'A#6/Bb6', solfege_name: 'La#6/Sib6', frequency: 1864.66},
+	93: {organ_key_number: 58, piano_key_number: 73, note_name: 'A6', solfege_name: 'La6', frequency: 1760},
+	92: {organ_key_number: 57, piano_key_number: 72, note_name: 'G#6/Ab6', solfege_name: 'Sol#6/Lab6', frequency: 1661.22},
+	91: {organ_key_number: 56, piano_key_number: 71, note_name: 'G6', solfege_name: 'Sol6', frequency: 1567.98},
+	90: {organ_key_number: 55, piano_key_number: 70, note_name: 'F#6/Gb6', solfege_name: 'Fa#6/Solb6', frequency: 1479.98},
+	89: {organ_key_number: 54, piano_key_number: 69, note_name: 'F6', solfege_name: 'Fa6', frequency: 1396.91},
+	88: {organ_key_number: 53, piano_key_number: 68, note_name: 'E6', solfege_name: 'Mi6', frequency: 1318.51},
+	87: {organ_key_number: 52, piano_key_number: 67, note_name: 'D#6/Eb6', solfege_name: 'Re#6/Mib6', frequency: 1244.51},
+	86: {organ_key_number: 51, piano_key_number: 66, note_name: 'D6', solfege_name: 'Re6', frequency: 1174.66},
+	85: {organ_key_number: 50, piano_key_number: 65, note_name: 'C#6/Db6', solfege_name: 'Do#6/Reb6', frequency: 1108.73},
+	84: {organ_key_number: 49, piano_key_number: 64, note_name: 'C6', solfege_name: 'Do6', frequency: 1046.5},
+	83: {organ_key_number: 48, piano_key_number: 63, note_name: 'B5', solfege_name: 'Si5', frequency: 987.77},
+	82: {organ_key_number: 47, piano_key_number: 62, note_name: 'A#5/Bb5', solfege_name: 'La#5/Sib5', frequency: 932.33},
+	81: {organ_key_number: 46, piano_key_number: 61, note_name: 'A5', solfege_name: 'La5', frequency: 880},
+	80: {organ_key_number: 45, piano_key_number: 60, note_name: 'G#5/Ab5', solfege_name: 'Sol#5/Lab5', frequency: 830.61},
+	79: {organ_key_number: 44, piano_key_number: 59, note_name: 'G5', solfege_name: 'Sol5', frequency: 783.99},
+	78: {organ_key_number: 43, piano_key_number: 58, note_name: 'F#5/Gb5', solfege_name: 'Fa#5/Solb5', frequency: 739.99},
+	77: {organ_key_number: 42, piano_key_number: 57, note_name: 'F5', solfege_name: 'Fa5', frequency: 698.46},
+	76: {organ_key_number: 41, piano_key_number: 56, note_name: 'E5', solfege_name: 'Mi5', frequency: 659.26},
+	75: {organ_key_number: 40, piano_key_number: 55, note_name: 'D#5/Eb5', solfege_name: 'Re#5/Mib5', frequency: 622.25},
+	74: {organ_key_number: 39, piano_key_number: 54, note_name: 'D5', solfege_name: 'Re5', frequency: 587.33},
+	73: {organ_key_number: 38, piano_key_number: 53, note_name: 'C#5/Db5', solfege_name: 'Do#5/Reb5', frequency: 554.37},
+	72: {organ_key_number: 37, piano_key_number: 52, note_name: 'C5', solfege_name: 'Do5', frequency: 523.25},
+	71: {organ_key_number: 36, piano_key_number: 51, note_name: 'B4', solfege_name: 'Si4', frequency: 493.88},
+	70: {organ_key_number: 35, piano_key_number: 50, note_name: 'A#4/Bb4', solfege_name: 'La#4/Sib4', frequency: 466.16},
+	69: {organ_key_number: 34, piano_key_number: 49, note_name: 'A4', solfege_name: 'La4', frequency: 440},
+	68: {organ_key_number: 33, piano_key_number: 48, note_name: 'G#4/Ab4', solfege_name: 'Sol#4/Lab4', frequency: 415.3},
+	67: {organ_key_number: 32, piano_key_number: 47, note_name: 'G4', solfege_name: 'Sol4', frequency: 392},
+	66: {organ_key_number: 31, piano_key_number: 46, note_name: 'F#4/Gb4', solfege_name: 'Fa#4/Solb4', frequency: 369.99},
+	65: {organ_key_number: 30, piano_key_number: 45, note_name: 'F4', solfege_name: 'Fa4', frequency: 349.23},
+	64: {organ_key_number: 29, piano_key_number: 44, note_name: 'E4', solfege_name: 'Mi4', frequency: 329.63},
+	63: {organ_key_number: 28, piano_key_number: 43, note_name: 'D#4/Eb4', solfege_name: 'Re#4/Mib4', frequency: 311.13},
+	62: {organ_key_number: 27, piano_key_number: 42, note_name: 'D4', solfege_name: 'Re4', frequency: 293.66},
+	61: {organ_key_number: 26, piano_key_number: 41, note_name: 'C#4/Db4', solfege_name: 'Do#4/Reb4', frequency: 277.18},
+	60: {organ_key_number: 25, piano_key_number: 40, note_name: 'C4', solfege_name: 'Do4', frequency: 261.63},
+	59: {organ_key_number: 24, piano_key_number: 39, note_name: 'B3', solfege_name: 'Si3', frequency: 246.94},
+	58: {organ_key_number: 23, piano_key_number: 38, note_name: 'A#3/Bb3', solfege_name: 'La#3/Sib3', frequency: 233.08},
+	57: {organ_key_number: 22, piano_key_number: 37, note_name: 'A3', solfege_name: 'La3', frequency: 220},
+	56: {organ_key_number: 21, piano_key_number: 36, note_name: 'G#3/Ab3', solfege_name: 'Sol#3/Lab3', frequency: 207.65},
+	55: {organ_key_number: 20, piano_key_number: 35, note_name: 'G3', solfege_name: 'Sol3', frequency: 196},
+	54: {organ_key_number: 19, piano_key_number: 34, note_name: 'F#3/Gb3', solfege_name: 'Fa#3/Solb3', frequency: 185},
+	53: {organ_key_number: 18, piano_key_number: 33, note_name: 'F3', solfege_name: 'Fa3', frequency: 174.61},
+	52: {organ_key_number: 17, piano_key_number: 32, note_name: 'E3', solfege_name: 'Mi3', frequency: 164.81},
+	51: {organ_key_number: 16, piano_key_number: 31, note_name: 'D#3/Eb3', solfege_name: 'Re#3/Mib3', frequency: 155.56},
+	50: {organ_key_number: 15, piano_key_number: 30, note_name: 'D3', solfege_name: 'Re3', frequency: 146.83},
+	49: {organ_key_number: 14, piano_key_number: 29, note_name: 'C#3/Db3', solfege_name: 'Do#3/Reb3', frequency: 138.59},
+	48: {organ_key_number: 13, piano_key_number: 28, note_name: 'C3', solfege_name: 'Do3', frequency: 130.81},
+	47: {organ_key_number: 12, piano_key_number: 27, note_name: 'B2', solfege_name: 'Si2', frequency: 123.47},
+	46: {organ_key_number: 11, piano_key_number: 26, note_name: 'A#2/Bb2', solfege_name: 'La#2/Sib2', frequency: 116.54},
+	45: {organ_key_number: 10, piano_key_number: 25, note_name: 'A2', solfege_name: 'La2', frequency: 110},
+	44: {organ_key_number: 9, piano_key_number: 24, note_name: 'G#2/Ab2', solfege_name: 'Sol#2/Lab2', frequency: 103.83},
+	43: {organ_key_number: 8, piano_key_number: 23, note_name: 'G2', solfege_name: 'Sol2', frequency: 98},
+	42: {organ_key_number: 7, piano_key_number: 22, note_name: 'F#2/Gb2', solfege_name: 'Fa#2/Solb2', frequency: 92.5},
+	41: {organ_key_number: 6, piano_key_number: 21, note_name: 'F2', solfege_name: 'Fa2', frequency: 87.31},
+	40: {organ_key_number: 5, piano_key_number: 20, note_name: 'E2', solfege_name: 'Mi2', frequency: 82.41},
+	39: {organ_key_number: 4, piano_key_number: 19, note_name: 'D#2/Eb2', solfege_name: 'Re#2/Mib2', frequency: 77.78},
+	38: {organ_key_number: 3, piano_key_number: 18, note_name: 'D2', solfege_name: 'Re2', frequency: 73.42},
+	37: {organ_key_number: 2, piano_key_number: 17, note_name: 'C#2/Db2', solfege_name: 'Do#2/Reb2', frequency: 69.3},
+	36: {organ_key_number: 1, piano_key_number: 16, note_name: 'C2', solfege_name: 'Do2', frequency: 65.41},
+	35: {organ_key_number: 0, piano_key_number: 15, note_name: 'B1', solfege_name: 'Si1', frequency: 61.74},
+	34: {organ_key_number: 0, piano_key_number: 14, note_name: 'A#1/Bb1', solfege_name: 'La#1/Sib1', frequency: 58.27},
+	33: {organ_key_number: 0, piano_key_number: 13, note_name: 'A1', solfege_name: 'La1', frequency: 55},
+	32: {organ_key_number: 0, piano_key_number: 12, note_name: 'G#1/Ab1', solfege_name: 'Sol#1/Lab1', frequency: 51.91},
+	31: {organ_key_number: 0, piano_key_number: 11, note_name: 'G1', solfege_name: 'Sol1', frequency: 49},
+	30: {organ_key_number: 0, piano_key_number: 10, note_name: 'F#1/Gb1', solfege_name: 'Fa#1/Solb1', frequency: 46.25},
+	29: {organ_key_number: 0, piano_key_number: 9, note_name: 'F1', solfege_name: 'Fa1', frequency: 43.65},
+	28: {organ_key_number: 0, piano_key_number: 8, note_name: 'E1', solfege_name: 'Mi1', frequency: 41.2},
+	27: {organ_key_number: 0, piano_key_number: 7, note_name: 'D#1/Eb1', solfege_name: 'Re#1/Mib1', frequency: 38.89},
+	26: {organ_key_number: 0, piano_key_number: 6, note_name: 'D1', solfege_name: 'Re1', frequency: 36.71},
+	25: {organ_key_number: 0, piano_key_number: 5, note_name: 'C#1/Db1', solfege_name: 'Do#1/Reb1', frequency: 34.65},
+	24: {organ_key_number: 0, piano_key_number: 4, note_name: 'C1', solfege_name: 'Do1', frequency: 32.7},
+	23: {organ_key_number: 0, piano_key_number: 3, note_name: 'B0', solfege_name: 'Si0', frequency: 30.87},
+	22: {organ_key_number: 0, piano_key_number: 2, note_name: 'A#0/Bb0', solfege_name: 'La#0/Sib0', frequency: 29.14},
+	21: {organ_key_number: 0, piano_key_number: 1, note_name: 'A0', solfege_name: 'La0', frequency: 27.5},
+}
+
+function getNoteName(midi_number, solfege=false)
+{
+	if (solfege==true)
+	{
+		return midi_notes[midi_number]["solfege_name"];
+	}
+	return midi_notes[midi_number]["note_name"];
+}
+
 function getRandomNumberInRange(min, max)
 {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -10,7 +129,7 @@ function getRandomNumberInList(lst)
 
 function showStaff(clef_type, notes)
 {
-	// Create a new staff
+	// Create a new Staff object
 	var myStaff = new Staff({
 		id: "myStaff",
 		at: "staffDiv",
@@ -18,13 +137,24 @@ function showStaff(clef_type, notes)
 		accidental: "sharp",
 		color: "#000000"
 	});
-	// Set notes (C major chord)
+	// Set notes
 	myStaff.setNotes(notes);
+	// Return object
+	return myStaff;
+}
+
+function showAnswer(note_number)
+{
+	$(".note-name").text(getNoteName(note_number));
+	$(".solfege-name").text(getNoteName(note_number, true));
 }
 
 function getRandomNote()
 {
-	$("#staffDiv").html(""); // Clear old staff
+	// Clear old staff and old answer
+	$("#staffDiv").html(""); 
+	$(".note-name").text("");
+	$(".solfege-name").text("");
 
 	var clef_type = "treble";
 	var accidental = false;
@@ -52,11 +182,15 @@ function getRandomNote()
 		}
 	}
 
-	showStaff(clef_type, notes);
+	var staff = showStaff(clef_type, notes);
+	var current_note = staff.getNotes()[0];
+	setTimeout(function() {
+		showAnswer(current_note);
+	}, 1000);
 }
 
 // Main
 $(document).ready(function() {
 	getRandomNote();
-	setInterval(getRandomNote, 1000);
+	setInterval(getRandomNote, 2000);
 });
